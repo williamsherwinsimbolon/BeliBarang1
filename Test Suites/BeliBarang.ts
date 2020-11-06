467e4a251ef26ab4bad07483546260b58cdac399
ADMIN <ADMIN@DESKTOP-UECTK66>
2020-11-06 13:42:45 +0700

    BeliBarang

--- Test Suites/BeliBarang.ts
+++ Test Suites/BeliBarang.ts
@@ -11,4 +11,10 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>e3881854-5d17-4fac-92ad-76757d680b93</testSuiteGuid>
+   <testCaseLink>
+      <guid>950b4ebd-956e-40d0-9735-6379bafbc577</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BeliBarang1</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>

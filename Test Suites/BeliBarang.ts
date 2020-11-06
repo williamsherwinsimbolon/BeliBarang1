978cc675f2984fbdeb7f4942f004b0c62bc90f23
ADMIN <ADMIN@DESKTOP-UECTK66>
2020-11-06 12:43:34 +0700

    Beli Barang

---
+++ Test Suites/BeliBarang.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BeliBarang</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e3881854-5d17-4fac-92ad-76757d680b93</testSuiteGuid>
+</TestSuiteEntity>

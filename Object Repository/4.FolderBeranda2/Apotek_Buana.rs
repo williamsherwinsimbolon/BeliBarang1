acd5855bc1fb95b0f530c995de373a5a2bed1268
ADMIN <ADMIN@DESKTOP-UECTK66>
2020-11-06 12:15:49 +0700

    tes

---
+++ Object Repository/4.FolderBeranda2/Apotek_Buana.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Apotek_Buana</name>
+   <tag></tag>
+   <elementGuidId>f1d42c2b-1034-4f1a-9fe8-34b483963d1a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//img[@src=&quot;https://img.goapotik.com/mediums/bks_apt_buana_1.jpg&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>

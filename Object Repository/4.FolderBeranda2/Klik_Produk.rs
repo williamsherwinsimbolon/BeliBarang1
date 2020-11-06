364065ca6e9396c624cbebb4ad42646c63b01e65
ADMIN <ADMIN@DESKTOP-UECTK66>
2020-11-06 12:16:02 +0700

    tes

---
+++ Object Repository/4.FolderBeranda2/Klik_Produk.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Klik_Produk</name>
+   <tag></tag>
+   <elementGuidId>36a411be-148a-45fd-9717-0a05fe187875</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[@class=&quot;nav-item step-merchant]/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
